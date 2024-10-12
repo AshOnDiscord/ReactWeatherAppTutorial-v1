140eeda6ab0e9f071be38d6786e20540384c083e
Ash <68016221+AshOnDiscord@users.noreply.github.com>
2024-10-12 23:01:55 +0000

    Fetch Data

--- src/App.tsx
+++ src/App.tsx
@@ -56,7 +56,51 @@ function App() {
   useEffect(() => {
     console.log("Location", currentLocation);
     if (!currentLocation) return;
+    console.log(
+      "lat:",
+      currentLocation.latitude,
+      "long:",
+      currentLocation.longitude,
+    );
+    fetchWeather();
   }, [currentLocation]);
+
+  const fetchWeather = async () => {
+    const params = {
+      latitude: [currentLocation?.latitude],
+      longitude: [currentLocation?.longitude],
+      daily: "weather_code,temperature_2m_max,temperature_2m_min",
+    };
+    const responses = await fetchWeatherApi(WEATHER_URL, params);
+
+    // Helper function to form time ranges
+    const range = (start: number, stop: number, step: number) =>
+      Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+    // Process first location. Add a for-loop for multiple locations or weather models
+    const response = responses[0];
+
+    // Attributes for timezone and location
+    const utcOffsetSeconds = response.utcOffsetSeconds();
+
+    const daily = response.daily()!;
+
+    // Note: The order of weather variables in the URL query and the indices below need to match!
+    const weatherData = {
+      daily: {
+        time: range(
+          Number(daily.time()),
+          Number(daily.timeEnd()),
+          daily.interval(),
+        ).map((t) => new Date((t + utcOffsetSeconds) * 1000)),
+        weatherCode: daily.variables(0)!.valuesArray()!,
+        temperatureMax: daily.variables(1)!.valuesArray()!,
+        temperatureMin: daily.variables(2)!.valuesArray()!,
+      },
+    };
+
+    console.log(weatherData);
+  };
 
   return (
     <>
